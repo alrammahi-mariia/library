c5d0c9ecff5a7a22e2b35ab2a6f1acd6b4046ca2
Mariia <mariia.bieskova@gmail.com>
2024-05-24 14:33:09 +0300

    write a function that loops through books array and displays each book in a separate card

---
+++ library.js
@@ -0,0 +1,45 @@
+const myLibrary = [];
+
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    }
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+
+const book1 = new Book('Hobbit', 'J.R.R.Tolkien', 345, 'read');
+const book2 = new Book('Big Little Lies', 'L.Moriarty', 456, 'not read yet');
+const book3 = new Book('Nightingale', 'K.Hannah', 434, 'read');
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+const container = document.querySelector('.container');
+const bookCard = document.createElement('div.book-card');
+
+
+myLibrary.map((item) => {
+    const bookCard = document.createElement('div')
+    bookCard.setAttribute('class','book-card')
+    const title = document.createElement('div')
+    const author = document.createElement('div')
+    const pages = document.createElement('div')
+    const read = document.createElement('div')
+    
+    container.appendChild(bookCard)
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    bookCard.appendChild(read)
+    
+    title.textContent = item.title
+    author.textContent = item.author
+    pages.textContent = item.pages
+    read.textContent = item.read
+ });
