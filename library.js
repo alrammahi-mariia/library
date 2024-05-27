a5b5efdb7c41aa6c76aaa1d1300ca8f180050c10
Mariia <mariia.bieskova@gmail.com>
2024-05-27 12:03:10 +0300

    resolve the issue with displaying cards, set myLibrary array to empty after the card is added to prevent repeating display of the same book;clear the form fields upon the form submission

--- library.js
+++ library.js
@@ -1,16 +1,24 @@
-const myLibrary = [];
+let myLibrary = [];
 
 
-// function Book(title, author, pages, read){
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-//     }
+function Book(title, author, pages, read, index){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.index = index;
+    }
 
-// function addBookToLibrary(book){
-//     myLibrary.push(book);
-// }
+function UserBook(index){
+    this.index = index;
+
+}
+
+function addBookToLibrary(Book){
+    if(Book){
+    myLibrary.push(Book);
+    }
+}
 
 // const book1 = new Book('Hobbit', 'J.R.R.Tolkien', 345, 'read');
 // const book2 = new Book('Big Little Lies', 'L.Moriarty', 456, 'not read yet');
@@ -25,33 +33,39 @@ const container = document.querySelector('.container');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const fd = new FormData(form);
-    const book = Object.fromEntries(fd);
-    myLibrary.push(book);
+    const fb = Object.fromEntries(fd);
+    addBookToLibrary(fb);
     console.log(myLibrary);
-    
-    myLibrary.forEach((item) => {
-       
+    displayCard();
+    clearForm(form);
+    myLibrary = [];
+});
+   
+function displayCard() {
+        myLibrary.forEach(item => {
         const bookCard = document.createElement('div')
         bookCard.setAttribute('class','book-card')
         const title = document.createElement('div')
         const author = document.createElement('div')
         const pages = document.createElement('div')
         const read = document.createElement('div')
+
+        title.textContent = item.title
+        author.textContent = item.author
+        pages.textContent = item.pages
+        read.textContent = item.read
     
         container.appendChild(bookCard)
         bookCard.appendChild(title)
         bookCard.appendChild(author)
         bookCard.appendChild(pages)
         bookCard.appendChild(read)
-        
-        title.textContent = item.title
-        author.textContent = item.author
-        pages.textContent = item.pages
-        read.textContent = item.read
-     });
+        })
+}
 
-})
-
-
-
+function clearForm(form){
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach(input => input.value = '');
+};
