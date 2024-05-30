8b694095d92be6f4181a36ef94261965c2a58cf2
Mariia <mariia.bieskova@gmail.com>
2024-05-30 13:59:29 +0300

    change read status to select option in the form and add styling to cards and a form

--- library.js
+++ library.js
@@ -29,7 +29,7 @@ form.addEventListener('submit', (e) => {
     let title = document.querySelector('input[name=title]').value;
     let author = document.querySelector('input[name=author]').value;
     let pages = document.querySelector('input[name=pages]').value;
-    let isRead = document.querySelector('input[name=read]').value;
+    let isRead = document.querySelector('select[name=read]').value;
 
     addBookToLibrary(title, author, pages, isRead);
     console.log(myLibrary);
@@ -48,45 +48,49 @@ function displayCards() {
         bookCard.setAttribute('class','book-card');
 
         let title = document.createElement('div');
+        title.setAttribute('class','card-title');
         title.textContent = item.title;
         bookCard.appendChild(title);
 
         let author = document.createElement('div');
+        author.setAttribute('class','card-subtitle');
         author.textContent = item.author;
         bookCard.appendChild(author);
 
         let pages = document.createElement('div');
+        pages.setAttribute('class','card-body');
         pages.textContent = item.pages;
         bookCard.appendChild(pages);
 
         let read = document.createElement('div');
+        read.setAttribute('class','card-body');
         read.textContent = item.isRead === true ? 'Read' : 'Not read';
         bookCard.appendChild(read);
 
+        let footer = document.createElement('div');
+        footer.setAttribute('class', 'card-footer');
+        bookCard.appendChild(footer);
 
         let toggleBtn = document.createElement('button');
+        toggleBtn.setAttribute('class', 'btn-secondary');
         toggleBtn.textContent = "Set status";
         toggleBtn.addEventListener('click', () => {
             item.toggleRead();
             displayCards();
         });
-        bookCard.appendChild(toggleBtn);
+        footer.appendChild(toggleBtn);
 
 
         let deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('class', 'btn-primary');
         deleteBtn.textContent = "Remove";
         deleteBtn.addEventListener('click', () => {
             myLibrary.splice(index, 1);
             displayCards();
         });
-        bookCard.appendChild(deleteBtn);
+        footer.appendChild(deleteBtn);
         
         container.appendChild(bookCard);
         })
    
     }
-
-// function clearForm(form){
-//     const inputs = form.querySelectorAll('input');
-//     inputs.forEach(input => input.value = '');
-// };
